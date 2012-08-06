9a6208118093a13fafec4b824981dcec518262bc
Carlos Arcenas <carlosarcenas@yahoo.com>
2012-08-06 19:05:35 +0700

    Added placeholder happies

---
+++ SciFair/SFTestFinishedViewController.h
@@ -0,0 +1,13 @@
+//
+//  SFTestFinishedViewController.h
+//  SciFair
+//
+//  Created by Carlos Arcenas on 8/6/12.
+//  Copyright (c) 2012 Carlos Arcenas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SFTestFinishedViewController : UIViewController
+
+@end
